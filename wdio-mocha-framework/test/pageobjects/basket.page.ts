8643929288a9e6c5200150e5a0417131ae01fad7
puzevich <puzevich.svetlana@gmail.com>
2026-06-25 19:01:31 +0300

    feat: basket tests

---
+++ wdio-mocha-framework/test/pageobjects/basket.page.ts
@@ -0,0 +1,101 @@
+import Page from './page';
+
+class BasketPage extends Page {
+  open() {
+    return super.open('checkout');
+  }
+
+  get productRows() {
+    return $$('[data-test="cart-item"]');
+  }
+
+  get productNames() {
+    return $$('[data-test="product-title"]');
+  }
+
+  get productPrices() {
+    return $$('[data-test="product-price"]');
+  }
+
+  get quantityInputs() {
+    return $$('input[data-test="product-quantity"]');
+  }
+
+  get itemTotals() {
+    return $$('[data-test="line-price"]');
+  }
+
+  get totalPrice() {
+    return $('[data-test="cart-total"]');
+  }
+
+  get continueShoppingButton() {
+    return $('button*=Continue Shopping');
+  }
+
+  get checkoutButton() {
+    return $('button*=Proceed to checkout');
+  }
+
+  get removeButtons() {
+    return $$('.btn-danger');
+  }
+
+  get emptyBasketMessage() {
+    return $('//div[contains(., "cart is empty")]');
+  }
+
+  get successAlert() {
+    return $('.alert');
+  }
+
+  async getItemTotal(index: number) {
+    const text = await this.itemTotals[index].getText();
+
+    return Number(text.replace(/[^\d.]/g, ''));
+  }
+
+  async getBasketTotal() {
+    const text = await this.totalPrice.getText();
+
+    return Number(text.replace(/[^\d.]/g, ''));
+  }
+
+  async setQuantity(index: number, quantity: number) {
+    const input = this.quantityInputs[index];
+
+    await input.clearValue();
+    await input.setValue(quantity.toString());
+
+    await browser.keys('Tab');
+  }
+
+  async removeProduct(index: number) {
+    await this.removeButtons[index].click();
+  }
+
+  async removeAllProducts() {
+    try {
+      await browser.waitUntil(async () => (await this.removeButtons.length) > 0, {
+        timeout: 3000,
+        timeoutMsg: 'There are no buttons',
+      });
+    } catch (error) {
+      return;
+    }
+
+    while ((await this.removeButtons.length) > 0) {
+      const firstButton = this.removeButtons[0];
+
+      await firstButton.waitForClickable({ timeout: 2000 });
+      await firstButton.click();
+
+      await browser.waitUntil(async () => !(await firstButton.isExisting()), {
+        timeout: 5000,
+        timeoutMsg: 'Remove button did not disappear after click',
+      });
+    }
+  }
+}
+
+export default new BasketPage();
--- wdio-mocha-framework/test/pageobjects/product.page.ts
+++ wdio-mocha-framework/test/pageobjects/product.page.ts
@@ -8,6 +8,15 @@ class ProductPage extends Page {
   get addToFavoritesButton() {
     return $('#btn-add-to-favorites');
   }
+
+  get addProductToBasketButton() {
+    return $('#btn-add-to-cart');
+  }
+
+  async addProductToBasket() {
+    await this.addProductToBasketButton.click();
+    await this.toastMessage.waitForDisplayed({ timeout: 3000 });
+  }
 }
 
 export default new ProductPage();
