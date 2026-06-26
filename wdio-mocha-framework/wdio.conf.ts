9b7a2a8ad331d6a3f35002155dde68c2d8d4a7df
puzevich <puzevich.svetlana@gmail.com>
2026-06-26 15:41:31 +0300

    fix: wdio config

--- wdio-mocha-framework/wdio.conf.ts
+++ wdio-mocha-framework/wdio.conf.ts
@@ -22,8 +22,9 @@ export const config: WebdriverIO.Config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ['./test/specs/**/*.ts'],
+  // specs: ['./test/specs/**/*.ts'],
   // Patterns to exclude.
+  specs: ['./test/specs/**/basket.ts'],
   exclude: [
     // 'path/to/excluded/files'
   ],
@@ -43,7 +44,7 @@ export const config: WebdriverIO.Config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: 3,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -65,7 +66,7 @@ export const config: WebdriverIO.Config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'error',
   //
   // Set specific log levels per logger
   // loggers:
