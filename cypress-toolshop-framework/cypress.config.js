cca8d9210a95387183c2f78eacefea1373d31ce0
puzevich <puzevich.svetlana@gmail.com>
2026-07-10 12:39:23 +0300

    init: cypress

---
+++ cypress-toolshop-framework/cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: 'https://practicesoftwaretesting.com/',
+    retries: { runMode: 2, openMode: 0 },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' || browser.name === 'electron') {
+          if (browser.isHeadless) {
+            launchOptions.args.push('--window-size=1920,1080');
+          }
+        }
+
+        if (browser.name === 'firefox' && browser.isHeadless) {
+          launchOptions.args.push('-width=1920');
+          launchOptions.args.push('-height=1080');
+        }
+
+        return launchOptions;
+      });
+
+      return config;
+    },
+  },
+});
